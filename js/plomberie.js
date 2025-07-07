6b5f7599c29f7abf1bce9d3199ad4b100e1b1bad
Thomas8008 <148946181+Thomas8008@users.noreply.github.com>
2025-07-07 21:41:36 +0200

    Add files via upload

--- js/plomberie.js
+++ js/plomberie.js
@@ -14,7 +14,40 @@
         }
       });
     
+document.addEventListener("DOMContentLoaded", function () {
+  const btn = document.getElementById("btn-contact");
+  const widget = document.getElementById("calendly-widget");
+
+  let isOpen = false;
+
+  if (btn && widget) {
+    // Clique sur le bouton
+    btn.addEventListener("click", function (e) {
+      e.preventDefault();
+      widget.style.display = isOpen ? "none" : "block";
+      isOpen = !isOpen;
+
+      if (isOpen) {
+        window.scrollTo({
+          top: widget.offsetTop,
+          behavior: "smooth"
+        });
+      }
+    });
+
+    // Clique ailleurs que le widget ou le bouton
+    document.addEventListener("click", function (e) {
+      const isClickInside =
+        widget.contains(e.target) || btn.contains(e.target);
+      if (!isClickInside && isOpen) {
+        widget.style.display = "none";
+        isOpen = false;
+      }
+    });
+  }
+});
 
 
+/* Fin de widget de badge Calendly */
 
-
+window.onload = function() { Calendly.initBadgeWidget({ url: 'https://calendly.com/tleclercq87/30min', text: '📅📱', color: '#1a1a1a', textColor: '#ffffff' }); }
